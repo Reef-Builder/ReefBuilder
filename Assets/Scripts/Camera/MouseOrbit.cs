cc8792795cbd0f090b714b370793f8056f979d7c
Sam-Costigan <samdcostigan@gmail.com>
2016-05-05 21:50:00 +1200

    Add drag handler script and UI with basic coral image

--- Assets/Scripts/Camera/MouseOrbit.cs
+++ Assets/Scripts/Camera/MouseOrbit.cs
@@ -17,6 +17,8 @@ public class MouseOrbit : MonoBehaviour
     public float distanceMin = 5f;
     public float distanceMax = 40f;
 
+	public bool dragging = false;
+
     private Rigidbody rigidbody;
 
     float x = 0.0f;
@@ -67,7 +69,7 @@ public class MouseOrbit : MonoBehaviour
 
     private void OrbitMouse(Transform target)
     {
-        if (target)
+		if (target && !dragging)
         {
             if (Input.GetMouseButton(0))
             {
---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject draggedObject;
+	public Vector3 startPosition;
+	public MouseOrbit mouseOrbitScript;
+
+	void Awake () {
+		mouseOrbitScript = GameObject.Find ("Main Camera").GetComponent<MouseOrbit> ();
+	}
+
+	public void OnBeginDrag (PointerEventData eventData) {
+		mouseOrbitScript.dragging = true;
+		draggedObject = gameObject;
+		startPosition = transform.position;
+	}
+
+	public void OnDrag (PointerEventData eventData) {
+		transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag (PointerEventData eventData) {
+		mouseOrbitScript.dragging = false;
+		draggedObject = null;
+		transform.position = startPosition;
+	}
+}
