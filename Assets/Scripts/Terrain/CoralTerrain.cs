40496d686f93343c0783d1a8e6af158460848fec
Tim King <lyrasitting@gmail.com>
2016-05-16 00:59:04 +1200

    Lower quality of android version (big FPS boost), add Up/Down controls (up/w = go up rock, down/s = go down rock)

--- Assets/Scripts/Terrain/CoralTerrain.cs
+++ Assets/Scripts/Terrain/CoralTerrain.cs
@@ -8,14 +8,29 @@ using System.Collections;
 
     Check whether objects have this script by checking that (object.GetComponent<CoralTerrain>() != null)
 
+    This script also allows the user to move the piece coral terrain lcoation around the reef that they are on, in
+    a maximum of two dimensions at the current time (up, down, left, right)
+
 */
 public class CoralTerrain : MonoBehaviour {
+
+    private Vector3 moveVec = new Vector3(0, 0.1f, 0);
+
+    public float maxHeight = 20;
+    public float minHeight = 0;
 
 	void Start() {
 		
 	}
 
-	void Update() {
-		
+	void FixedUpdate() {
+        print(Input.GetAxis("Vertical"));
+        if (Input.GetAxis("Vertical") > 0 && transform.position.y < maxHeight)
+        {
+            transform.Translate(moveVec);
+        } else if (Input.GetAxis("Vertical") < 0 && transform.position.y > minHeight)
+        {
+            transform.Translate(-moveVec);
+        }
 	}
 }
