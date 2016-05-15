4d97927ec8ced37d75effd2dc9c5f73317bedad8
GetHubMax <max.harry@hotmail.com>
2016-05-15 17:43:04 +1200

    Paths for Fish
    Found a pugin for the fish to follow paths. The is a basic test one I
    made but there are still problems. The next thing I will do it get the
    Animations working.

---
+++ Assets/Scripts/FishAI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishAI : MonoBehaviour {
+    private static int NORAML=0;
+    private static int EAT = 1;
+    private static int FLEA = 2;
+    public int state = NORAML;
+    public LayerMask foodChain = 0;
+    public Collider vision;
+
+    public float currentSpeed;
+    public float max_Speed;
+    public float nor_Speed;
+    public float fleaTime = 50;
+    private float timer = 0;
+    private bool inDanger = false;
+    private float radius = 0.05f;
+    private float zone = 5;
+    public Vector3 goal;
+
+    // Use this for initialization
+    void Start () {
+	//	iTween.RotateBy(gameObject, iTween.Hash("x", .25, "easeType", "easeInOutBack", "loopType", "pingPong", "delay", .4));
+
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("path00"),"speed",nor_Speed, "easetype",iTween.EaseType.linear, "orienttopath", true, "lookahead\t",.01f,"isLocal",true));    
+        
+    }
+
+    void FixedUpdate()
+	{
+		
+	}
+
+    
+
+   	// Update is called once per frame
+	void Update () {
+        
+         
+	}
+
+    void OnTriggerEnter(Collider c) {
+    
+
+    }
+
+    void OnTriggerExit() {
+       
+
+    }
+
+
+    
+
+}
