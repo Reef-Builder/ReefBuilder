81945fac6bef09aa0d7e2c6fab7f116c9e19ce6b
GetHubMax <max.harry@hotmail.com>
2016-05-17 14:27:37 +1200

    Added iTween
    Add iTween to the newest master branch. Also I believe the master branch
    work setup is on an older verison of Unity with I had to download.

---
+++ Assets/Scripts/PathFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public string path;
+	public float speed=1;
+
+	void Start () {
+		iTween.PutOnPath (gameObject, iTweenPath.GetPath (path), 0f);
+		iTween.MoveTo(gameObject,iTween.Hash("path", iTweenPath.GetPath(path),"speed",speed, "easeType",iTween.EaseType.linear, "Orienttopath", true, "looptype",iTween.LoopType.loop));
+	}
+
+	void FixedUpdate(){
+		
+
+
+	
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
