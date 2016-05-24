40c10d91e0a55ba68504fdeb74b941cad0f90d7d
Sam-Costigan <samdcostigan@gmail.com>
2016-05-24 15:12:59 +1200

    Add basic game menu, enable exiting game

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewGame() {
+		print ("start new game");
+	}
+
+	public void LoadGame() {
+		print ("you what");
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+}
