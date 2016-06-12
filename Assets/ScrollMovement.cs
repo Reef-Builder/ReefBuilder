6ed2e17dd940f017dc3808de0ac3243a3fd91c9a
Tim King <lyrasitting@gmail.com>
2016-06-12 21:54:49 +1200

    Add new fish and coral

---
+++ Assets/ScrollMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollMovement : MonoBehaviour {
+
+    public Vector3 moveVec;
+    private Vector3 startPosition;
+    private float mag;
+
+    public float moveSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+        mag = (moveVec - startPosition).magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += moveVec * moveSpeed * Time.deltaTime;
+	    if((transform.position - startPosition).magnitude >= mag) {
+            transform.position = startPosition;
+        }
+	}
+}
